0f2a8876935a6f43639d19bc8c7f8bfe2e2a9bf5
Konnov Ivan <konnov-ivan@yandex.ru>
2024-06-18 14:22:28 +0300

    HW3

--- myapp/management/commands/fake_db.py
+++ myapp/management/commands/fake_db.py
@@ -1,19 +1,79 @@
+import random
+
 from django.core.management.base import BaseCommand
-from myapp.models import User, Product, Order
+from myapp.models import User, Product, Order, OrderProducts
+from datetime import datetime, timedelta
+
+# максимум продуктов в заказе
+MAX_PROD_IN_ORDER = 10
+
+# максимум продуктов в наличии
+MAX_PRODUCTS = 100
+
+# Кол-во генерируемых записей в таблицах
+DEFAULT_CLIENTS = 10
+DEFAULT_PRODUCTS = 10
+DEFAULT_ORDERS = 10
+
+# Ценовой диапазон продуктов
+MIN_PRICE = 100
+MAX_PRICE = 1000
 
 
 class Command(BaseCommand):
-    help = "Generate fake Orders."
+    help = f"Заполнение базы тестовыми данными. Количество продукта в заказе от 1 до {MAX_PROD_IN_ORDER} включительно"
 
     def add_arguments(self, parser):
-        parser.add_argument('count', type=int, help='User ID')
+        parser.add_argument("-c", "--clients", type=int,
+                            help=f'Count of clients for generate. {DEFAULT_CLIENTS} on default.')
+        parser.add_argument("-p", "--products", type=int,
+                            help=f'Count of products for generate. {DEFAULT_PRODUCTS} on default.')
+        parser.add_argument("-o", "--orders", type=int,
+                            help=f'Count of orders for generate. {DEFAULT_ORDERS} on default per user.')
 
-    def handle(self, *args, **kwargs):
-        count = kwargs.get('count')
-        for i in range(1, count + 1):
-            order = Order(customer=f'Name{i}', products=f'mail{i}@mail.ru', all_price=i*543)
+    def handle(self, *args, **options):
+        clients_count = DEFAULT_CLIENTS if options['clients'] is None else options['clients']
+        products_count = DEFAULT_PRODUCTS if options['products'] is None else options['products']
+        orders_count = DEFAULT_ORDERS if options['orders'] is None else options['orders']
+
+        # генерация клиентов
+        customers = [
+            User(name=f'Client_{x}', email=f'client{x}@mail.ru', phone=None, adress=None)
+            for x in range(1, clients_count + 1)]
+        for customer in customers:
+            customer.save()
+
+        # генерация продуктов
+        products = [Product(name=f'Product {x}', descriptions=None, price=random.randint(MIN_PRICE, MAX_PRICE),
+                            count=random.randint(1, MAX_PRODUCTS))
+                    for x in range(1, products_count + 1)]
+        for prod in products:
+            prod.save()
+
+        # генерация заказов
+        orders = list()
+        for customer in customers:
+            for _ in range(orders_count):
+                order = Order.objects.create(customer=customer)
+                orders.append(order)
+
+        # заполнение заказов продуктами
+        start = datetime.strptime('01.01.2023', '%d.%m.%Y')
+        end = datetime.strptime('01.01.2024', '%d.%m.%Y')
+
+        for order in orders:
+            prod_in_order = random.randint(1, products_count)
+            order.order_date = self.get_random_date(start, end)
+            for _ in range(prod_in_order):
+                prod = random.choice(products)
+                # if OrderProducts.objects.filter(order_id=order.pk, product_id=prod.pk).first() is None:
+                prod_cnt = random.randint(1, MAX_PROD_IN_ORDER)
+                OrderProducts.objects.create(order=order, product=prod, product_count=prod_cnt)
+                order.all_price += prod_cnt * prod.price
             order.save()
-            for j in range(1, count + 1):
-                product = Product(name=f'Name{j}', descriptions=f'Text from {user.name} #{j} is bla bla bla many long text',
-                               price=j*345, count=j)
-                product.save()
+
+        self.stdout.write('Test data was created.')
+
+    def get_random_date(self, start, end):
+        delta = end - start
+        return start + timedelta(random.randint(0, delta.days))
--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,13 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя клиента')
     email = models.EmailField(max_length=50, verbose_name='Электронная почта клиента')
-    phone = models.IntegerField(verbose_name='Номер телефона клиента')
-    adress = models.CharField(max_length=150, verbose_name='Адрес клиента')
+    phone = PhoneNumberField(null=True, verbose_name='Номер телефона клиента')
+    adress = models.CharField(null=True, max_length=150, verbose_name='Адрес клиента')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации клиента')
 
     def __str__(self):
@@ -14,14 +16,19 @@ class User(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=30, verbose_name='Название товара')
-    descriptions = models.TextField(verbose_name='Описание товара')
+    descriptions = models.TextField(null=True, verbose_name='Описание товара')
     price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name=' Цена товара')
     count = models.IntegerField(verbose_name='Количество товара')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавленяе товара')
+
+    def __str__(self):
+        return f'Name is {self.name}'
 
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
-    all_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цены товара')
+    all_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)],
+                                   verbose_name='Цены товара')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавленяе товара')
+
